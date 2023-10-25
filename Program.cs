82d716cd0fa8a27131dead61748c79f04d984f0c
Ângelo Ferreira <112225868+angelloferreira@users.noreply.github.com>
2023-10-25 09:03:04 -0400

    main commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using membrosestaticos;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entre com o valor do Raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volu = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Volume: " + volu.ToString("F2", CultureInfo.InvariantCulture));
+            
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+        
+    }
+}
